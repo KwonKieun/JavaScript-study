0b5286e65c59ff75f9de05f4ac985d8f6872491e
KwonKieun <kgo6625@naver.com>
2023-01-15 16:17:09 +0900

    forEach

---
+++ day_03/js06.js
@@ -0,0 +1,8 @@
+const fruits = ['사과', '배', '귤', '바나나', '딸기']
+
+fruits.forEach((value)=>console.log(value)); // 해당 배열의 모든 요소에 대하여 반복적으로 명시된 콜백 함수를 실행
+/*
+for (let i = 0; i < fruits.length; i++){
+
+}
+*/
