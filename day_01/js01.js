1d7cc2bc23fc80db4fa8adc962d9158bb9ab2e31
KwonKieun <kgo6625@naver.com>
2023-01-15 15:26:20 +0900

    fin

---
+++ day_01/js01.js
@@ -0,0 +1,60 @@
+let a = 0;
+console.log(a);
+let b = 10;
+console.log(b);
+let c = 20;
+console.log(c);
+
+/*const d = "hello world";
+console.log(d) ;*/
+// d = "cng value" ; // const -> 바꿀 수 없음
+
+let num=50;
+let str = "자바스크립트";
+let value1 = 123+"2023";
+let value2 = 2022/"2023";
+console.log(num);
+console.log(str);
+console.log(typeof value1);
+console.log(typeof value2);
+
+let str1 = 2030;
+let str2 = "안녕\n"+str1+"년";
+console.log(str2);
+
+// 템플릿 리터럴 백틱 `
+let str3 = `안녕하세요
+올해는 ${str1}년입니다.`;
+console.log(str3);
+
+// boolean 불리언
+let isOn = true ;
+let isOff = false ;
+console.log(isOn);
+console.log(typeof isOff);
+
+let js1;
+console.log(typeof js1);
+
+// 자바스크립트에서 거짓(false)으로 취급하는 값
+// 0, -0, null, undefinded, NaN, ''
+
+// 연산자
+// 기본연산자
+// % 나머지연산자
+let num1 = 20;
+let num2 = 3;
+console.log(num1%num2);
+
+// 할당연산자
+let value3 = 1;
+value3 *= 10;
+console.log(value3); // 10
+value3 *= 10;
+console.log(value3); // 100
+value3 %= 10;
+console.log(value3); // 0
+
+// 비교연산자
+
+
